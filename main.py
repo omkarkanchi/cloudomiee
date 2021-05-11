ad9b602239ca905d82d330e212310f99bd1f91bd
omkarkanchi <84017915+omkarkanchi@users.noreply.github.com>
2021-05-11 16:51:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.write('---')
 st.sidebar.subheader('Query parameters')
 start_date = st.sidebar.date_input("Start date", datetime.date(2020, 1, 1))
 end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
-ticker_list = pd.read_csv('nifty50.csv')
+ticker_list = pd.read_csv('https://github.com/omkarkanchi/cloudomiee/blob/83319a263aee19677ee179b806488c4190082a63/nifty50.csv')
 yahoo_symbols = ticker_list.Yahoo_Symbol.to_list()
 tickerSymbol = st.sidebar.selectbox('Stock ticker', yahoo_symbols) # Select ticker symbol
 tickerData = yf.Ticker(tickerSymbol) # Get ticker data
